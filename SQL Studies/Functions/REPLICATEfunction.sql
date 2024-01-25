b79b48476f12aa76d490431f7437c9e352a03566
Patrick Desgrange <pdesgrange@gmail.com>
2024-01-25 07:23:13 -0500

    new file:   SQL Studies/Functions/REPLICATEfunction.sql

---
+++ SQL Studies/Functions/REPLICATEfunction.sql
@@ -0,0 +1,17 @@
+/*
+    Function: REPLICATE
+    Syntax: RREPLICATE(string, n)
+    Description: This function returns a string value that consists of a specified string repeated a specified number of times.
+    Common Uses: 
+    - Padding a string with a specific character or set of characters.
+    - Generating test data with repeated patterns.
+*/
+
+USE TSQLV6;
+GO
+
+SELECT Replicate('Hello', 3) AS Result;
+
+SELECT custid
+    , RIGHT(REPLICATE('0', 5) + CONVERT(VARCHAR(10), custid), 5) as empid_formatted
+FROM Sales.Customers
