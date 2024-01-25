581330882672b60e5267e7a2c5f486aa6f3a1361
Patrick Desgrange <pdesgrange@gmail.com>
2024-01-25 07:27:16 -0500

    modified:   SQL Studies/Functions/REPLICATEfunction.sql

--- SQL Studies/Functions/REPLICATEfunction.sql
+++ SQL Studies/Functions/REPLICATEfunction.sql
@@ -15,3 +15,10 @@ SELECT Replicate('Hello', 3) AS Result;
 SELECT custid
     , RIGHT(REPLICATE('0', 5) + CONVERT(VARCHAR(10), custid), 5) as empid_formatted
 FROM Sales.Customers
+
+
+--visualize data
+SELECT empid
+    , REPLICATE('*', empid) as chart
+FROM HR.Employees
+ORDER BY chart DESC
